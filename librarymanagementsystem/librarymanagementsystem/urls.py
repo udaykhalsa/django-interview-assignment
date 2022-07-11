5780442d017b99bcd5556ce38d440cbaac73db6a
Uday Singh Khalsa <udaykhalsa@gmail.com>
2022-07-11 11:19:33 +0530

    Add openapi docs

--- librarymanagementsystem/librarymanagementsystem/urls.py
+++ librarymanagementsystem/librarymanagementsystem/urls.py
@@ -1,16 +1,33 @@
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework_simplejwt import views as jwt_views
+from django.urls import path, include, re_path
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Library Management System",
+        default_version='v1',
+        description="This is library management system for True Value Access.",
+        contact=openapi.Contact(email="udaykhalsa@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    re_path(r'^doc(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
     path('admin/', admin.site.urls),
 
     path('', include('users.urls')),
     path('', include('management.urls')),
 
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(),
-         name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(),
-         name='token_refresh'),
 
 ]
